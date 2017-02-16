eb185027a8b4ef81252ca545a08d3096bbac5e60
Bobby Frazette <Bobby Frazette>
2017-02-15 23:28:45 -0500

    updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,77 +1,31 @@
-//create timer function
-//create start function
+
 //create game function
 //create clickable log function
 //create if then statements to log correct answers and score
-//create allert with score
 
-/*
-var intervalId;
-var running = false;
+var q1Answer;
+var q2Answer;
+var q3Answer;
+var q4Answer;
+var q5Answer;
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+var time = 5;
+//var userScore = 0;
+var countdown;
 
-var time = {
-	timeLeft: 0200,
-	//timeStart: function(){
-		timer: function(){
-			if(running == false){
-				intervalId = setInterval(time.count, 1000);
-				running = true;
-			}
-		},
-		count: function(){
-			time.timeLeft--;
-			var converted = time.timeConverter(time.timeLeft);
-			$("#timer").html(converted);
-			console.log(converted);
-		},
-		timeConverter: function(t) {
-			var minutes = Math.floor(t / 60);
-			var seconds = t - (minutes * 60);
-			if (seconds < 10) {
-				seconds = "0" + seconds;
-			}
-			if (minutes === 0) {
-				minutes = "00";
-			}
-			else if (minutes < 10) {
-				minutes = "0" + minutes;
-			}
-			return minutes + ":" + seconds;
-			}
-	//}
-};
-*/
+window.onload = function(){
+	$("#results").hide();
+	$("#trivia").hide();
+}
 
-/*
-var time = 0200;
-	function startTimer() {
-	intervalId = setInterval(count, 1000);
-	}
-	function count() {
-		time--;
-		var converted = timeConverter(time);
-		$("#timer").html(converted);
-	}
-	function timeConverter(t) {
-		var minutes = Math.floor(t / 60);
-		var seconds = t - (minutes * 60);
-		if (seconds < 10) {
-			seconds = "0" + seconds;
-		}
-		if (minutes === 0) {
-			minutes = "00";
-		}
-		else if (minutes < 10) {
-			minutes = "0" + minutes;
-		}
-		return minutes + ":" + seconds;
-		}
-*/
-
-function game(){
-	$("#startButton").on("click", function(event){
-		var userScore = 0;
+//var game = function(){
+	$("#startButton").on("click", function(){
 		function start(){
+//			$("#startButton").prop("disabled",true);
+			$("#startButton").hide();
+			$("#trivia").show();
 			$("#batman").html("1. Batman!");
 				$("#q1").html("What is Batman's alter ego?");
 					$("#q1a1").html(" A. Bruce Banner");
@@ -79,7 +33,7 @@ function game(){
 					$("#q1a3").html(" C. Bruce Wayne");
 					$("#q1a4").html(" D. Steve Rogers");
 			$("#captainamerica").html("2. Captain America!");
-				$("#q2").html("What year did Captain Americareceive his abilities?");
+				$("#q2").html("What year did Captain America receive his abilities?");
 					$("#q2a1").html(" A. 1939");
 					$("#q2a2").html(" B. 1941");
 					$("#q2a3").html(" C. 1943");
@@ -102,50 +56,28 @@ function game(){
 					$("#q5a2").html(" B. Bronx");
 					$("#q5a3").html(" C. Queens");
 					$("#q5a4").html(" D. Manhattan");
+			stopwatch();
+			countdown = setInterval(stopwatch,1000);
 		};
 		start();
 	});
-	var time = 5;
-	var running = false;
-	$("#startButton").on("click", function(event){
-			function startTimer() {
-				if(running == false){
-					intervalId = setInterval(count, 1000);
-					running = true;
-				};
-//				if(time <= 0){
-//					stopTimer();
-//				}
-			};
-			startTimer();
-			function stopTimer() {
-				if(running = true){
-					if(time == 0){
-					clearInterval(intervalId);
-					running = false;
-					};
-				};
-			stopTimer();
-			};
-			function count() {
-				time--;
-				var converted = timeConverter(time);
-				$("#timer").html(converted);
-			};
-			function timeConverter(t) {
-				var minutes = Math.floor(t / 60);
-				var seconds = t - (minutes * 60);
-				if (seconds < 10) {
-					seconds = "0" + seconds;
-				}
-				if (minutes === 0) {
-					minutes = "00";
-				}
-				else if (minutes < 10) {
-					minutes = "0" + minutes;
-				}
-				return minutes + ":" + seconds;
-				}
-	});
-};
-game();
+	var stopwatch = function(){
+			time--;
+			$("#timer").html(time + " Seconds");
+			stop();
+	};
+	var stop = function(){
+		if(time===0){
+			clearInterval(countdown);
+			alert("Time is up!");
+			$("#timer").hide();
+			$("#trivia").hide();
+			$("#results").show();
+			$("#done").html("All Done!");
+			$("#correct").html("Correct Answers: " + correct);
+			$("#incorrect").html("Incorrect Answers: " + incorrect);
+			$("#unanswered").html("Unanswered: " + unanswered);
+		}
+	}
+//};
+//game();
